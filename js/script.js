3bc955c8b9bde633fba2d7e8b72a81d95ed979ef
kobak-github <konurbaevbm@gmail.com>
2023-01-04 10:43:56 +0600

    task#4 modifying the modal window

--- js/script.js
+++ js/script.js
@@ -92,12 +92,15 @@ window.addEventListener('DOMContentLoaded', () => {
     modalCloseBtn = document.querySelector('[data-close]'),
     modal = document.querySelector('.modal');
 
+  function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+  }
+
   modalOpenBtn.forEach((btn) => {
-    btn.addEventListener('click', () => {
-      modal.classList.add('show');
-      modal.classList.remove('hide');
-      document.body.style.overflow = 'hidden';
-    });
+    btn.addEventListener('click', openModal);
   });
 
   function closeModal() {
@@ -119,4 +122,18 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }
   });
+
+  const modalTimerId = setTimeout(openModal, 6000);
+
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
 });
